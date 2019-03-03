d16029dfb1817feb17b3a488121de7498ab798cc
susan <bluenote@outlook.com>
2019-03-03 08:09:52 -0500

    css navbar

---
+++ src/Components/NavCss.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavCSS = () => {
+  return (
+   <nav class= "nav_block">
+    <ul className="nav_list">
+    {/*<div style={{borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}> */}
+      <NavLink
+
+        to="/"
+      >
+      <li className="nav_item"> Home </li>
+      </NavLink>
+      <NavLink
+
+        to="/main"
+        >
+      <li className="nav_item"> See parks and campgrounds </li>
+      </NavLink>
+
+      <NavLink
+
+        to="/login"
+        >
+      <li className="nav_item"> Login </li>
+      </NavLink>
+
+      <NavLink
+        to="/signup"
+      >
+
+      <li className="nav_item"> Register </li>
+      </NavLink>
+
+      <NavLink
+        to="/logout"
+
+      >
+      <li className="nav_item"> Logout </li>
+      </NavLink>
+      </ul>
+
+    </nav>
+  );
+}
+
+export default NavCSS;
+
+//style={{ marginRight: '10px' }}
